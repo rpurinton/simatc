e377a469cac47996130f0f2c983208035fba317a
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-01-11 19:43:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,17 +7,21 @@ $result = $sql->query("SELECT airport FROM (
 ) AS combined ORDER BY airport;");
 
 $airports = [];
+$fixes = [];
 while ($row = $result->fetch_assoc()) {
     $firstLetter = strtoupper(substr($row['airport'], 0, 1));
-    $airports[$firstLetter][] = $row['airport'];
+    if (strlen($row['airport']) == 3) {
+        $airports[$firstLetter][] = $row['airport'];
+    } elseif (strlen($row['airport']) == 5) {
+        $fixes[$firstLetter][] = $row['airport'];
+    }
 }
 ?>
-
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title>Airports</title>
+    <title>Airports and Fixes</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
@@ -30,11 +34,21 @@ while ($row = $result->fetch_assoc()) {
                 <?php endforeach; ?>
             </div>
         </div>
+        <h1>Airports</h1>
         <?php foreach ($airports as $letter => $airportNames) : ?>
             <div class="row" id="<?php echo $letter; ?>">
                 <div class="col-12">
                     <h2><?php echo $letter; ?></h2>
                     <p><?php echo implode(', ', $airportNames); ?></p>
+                </div>
+            </div>
+        <?php endforeach; ?>
+        <h1>Fixes</h1>
+        <?php foreach ($fixes as $letter => $fixNames) : ?>
+            <div class="row" id="<?php echo $letter; ?>">
+                <div class="col-12">
+                    <h2><?php echo $letter; ?></h2>
+                    <p><?php echo implode(', ', $fixNames); ?></p>
                 </div>
             </div>
         <?php endforeach; ?>
